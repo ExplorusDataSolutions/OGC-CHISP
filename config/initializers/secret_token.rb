4974e55b1971318732ab846b637651b53e4fc3da
Ma Lian <malhch@gmail.com>
2012-11-28 00:51:13 +0800

    Default rails new OGC-CHISP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OGCChisp::Application.config.secret_token = 'cb709597fa93cdb0df66289c8e5107d9da34b7cbf91acc2b863c99d2290a0d0be569c3d8b3896e1f1aa4fa1c024d51c882ddb0ab17f8b091e136ecf3dd6556ee'
