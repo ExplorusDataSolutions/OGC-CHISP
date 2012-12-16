f33dd5c7e01e2d4c3d64cee38d92796d9d1bcfe7
Ma Lian <malhch@gmail.com>
2012-12-16 20:51:29 +0800

    Bootstrap for SASS integrated, Leaflet.markercluster integrated

--- Gemfile
+++ Gemfile
@@ -8,12 +8,13 @@ gem 'rails', '3.2.8'
 gem 'sqlite3'
 gem 'libxml-ruby'
 
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
+  gem 'compass-rails'
+  gem 'bootstrap-sass', '~> 2.2.1.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,11 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+# If you want your assets lazily compiled in production, use this line
+# Bundler.require(:default, :assets, Rails.env)
 end
 
 module OGCChisp
@@ -59,6 +58,11 @@ module OGCChisp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
+    # For displaying 404 page
     config.exceptions_app = self.routes
+    
+    # This makes it clear that which plugins are involved
+    config.assets.paths << Rails.root.join("lib", "plugins", "leaflet", "dist")
+    config.assets.paths << Rails.root.join("lib", "plugins", "leaflet.markercluster", "dist")
   end
 end
