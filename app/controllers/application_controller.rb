0034343c8b84611bdf4f27453dbec474372c2e6a
Ma Lian <malhch@gmail.com>
2013-01-04 03:25:03 +0800

    Login section added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,22 @@ class ApplicationController < ActionController::Base
     self.content_type = "application/xml"
     self.response_body = %x{curl -G -d "#{csw_params}" "#{csw_service_url}"}
   end
+
+  def is_valid_email? value
+    begin
+      m = Mail::Address.new(value)
+      # We must check that value contains a domain and that value is an email address
+      r = m.domain && m.address == value ? true : false
+      t = m.__send__(:tree)
+      # We need to dig into treetop
+      # A valid domain must have dot_atom_text elements size > 1
+      # user@localhost is excluded
+      # treetop must respond to domain
+      # We exclude valid email values like <user@localhost.com>
+      # Hence we use m.__send__(tree).domain
+      r &&= (t.domain.dot_atom_text.elements.size > 1)
+    rescue Exception => e
+    r = false
+    end
+  end
 end
