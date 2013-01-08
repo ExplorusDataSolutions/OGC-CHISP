dbe7995c54ba47a4ecd4b523d151d60badbed204
Ma Lian <malhch@gmail.com>
2013-01-09 02:04:11 +0800

    Use the xml data of ED WMS endpoint to change the subscription status

--- app/controllers/csw_controller.rb
+++ app/controllers/csw_controller.rb
@@ -5,7 +5,26 @@ class CswController < ApplicationController
 
   def proxy
     self.content_type = "application/xml"
-    self.response_body = %x{curl -G "#{params['url']}"}
+    uri = URI.parse(params['url'])
+
+    if params["xml"]
+      response_body = Net::HTTP.post_form(uri, { "xml" => params["xml"] }).body
+    else
+      require 'net/http'
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == "200"
+      response_body = response.body
+      elsif response.code == "302"
+        # INFO_FORMAT is necessary for responsed xml data
+        redirect = response["location"] + "INFO_FORMAT=text/xml"
+        response_body = Net::HTTP.get_response(URI.parse(redirect)).body
+      else
+      response_body = response.inspect
+      end
+    end
+
+    self.response_body = response_body
   end
 
   def get_capabilities
